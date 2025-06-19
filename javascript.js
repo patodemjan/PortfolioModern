223cc4a0a76d5f555d112f7279395a635a5c331e
Patrik Demjan <155092026+patodemjan@users.noreply.github.com>
2025-06-19 17:33:52 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -48,27 +48,4 @@ const items = document.querySelectorAll('.img');
 
   loadShow();
 
-document.addEventListener("DOMContentLoaded", () => {
-  let visits = localStorage.getItem("visitCount");
-  visits = visits === null ? 1 : parseInt(visits) + 1;
-  localStorage.setItem("visitCount", visits);
 
-  const counterElement = document.getElementById("visitor-counter");
-  if (counterElement) {
-    counterElement.textContent = "Besucheranzahl: " + visits;
-  }
-
-  let n = localStorage.getItem("on_load_counter");
-  n = n === null ? 1 : parseInt(n) + 1;
-  localStorage.setItem("on_load_counter", n);
-
-  const nums = n.toString().split("").map(Number);
-  const counterVisitor = document.getElementById("CounterVisitor");
-
-  if (counterVisitor) {
-    counterVisitor.innerHTML = "";
-    nums.forEach(i => {
-      counterVisitor.innerHTML += `<span class="counter-item">${i}</span>`;
-    });
-  }
-});
